c86e2cfe33a18262201a86fee658fbf58ca131d8
Andrew Speciale <aspeciale@advisor360.com>
2023-09-12 16:24:06 -0400

    Added additional metadata information

--- setup.py
+++ setup.py
@@ -3,11 +3,44 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
+def read_files(files):
+    data = []
+    for file in files:
+        with open(file, encoding='utf-8') as f:
+            data.append(f.read())
+    return "\n".join(data)
+
+long_description = read_files('README.md')
+
+meta = {}
+with open('src/csv_compare_tool/version.py', encoding='utf-8') as f:
+    exec(f.read(), meta)
+
 setup(
-    name='csv-comparer',
-    author='liquidz00 // github.com/liquidz00',
-    version='1.0',
+    name='csv_compare_tool_liquidz00',
     description='Package to find differences between two CSV files',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version=meta['__version__'],
+    author='Andrew Speciale',
+    url="https://github.com/liquidz00/csv-compare",
     packages=find_packages(),
-    install_requires=requirements
+    python_requires=">=3.8",
+    install_requires=requirements,
+    license='Apache-2.0',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Mac Administrators',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: Apache-2.0 License',
+        'Operating System :: OS Independent'
+    ]
 )
